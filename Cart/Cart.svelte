5cab2bb82a7ffa57db2c81280250f17d69a8475c
@mantonoiu <antonoiumadalin@gmail.com>
2019-07-31 13:49:13 -0400

    stores 07 auto subscriptions Shortcut! $cartItems. that`s it. GG
    $cartItems removes all that code!!!!
    No longer needed all that code

--- Cart/Cart.svelte
+++ Cart/Cart.svelte
@@ -1,20 +1,6 @@
 <script>
-  import { onDestroy } from 'svelte';
   import cartItems from './cart-store';
   import CartItem from "./CartItem.svelte";
-
-  let items;
-  const unsubscribe = cartItems.subscribe( its => {
-    items = its;
-  }); 
-  //Always clear your subscriptions
-  onDestroy (() => {
-    if (unsubscribe){
-      unsubscribe();
-    }
-  });
-
-
 </script>
 
 <style>
@@ -35,7 +21,7 @@
 <section>
   <h1>Cart</h1>
   <ul>
-    {#each items as item (item.id)}
+    {#each $cartItems as item (item.id)}
       <CartItem id={item.id} title={item.title} price={item.price} />
     {:else}
       <p>No items in cart yet!</p>
